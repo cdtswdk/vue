2562121faf94ece26dc15a501f1e3994143f37e7
cdtswdk <1445676933@qq.com>
2020-12-07 20:48:25 +0800

    第7天 tab-bar 文件路径的引用问题

--- 04-tabbar/src/components/MainTabBar.vue
+++ 04-tabbar/src/components/MainTabBar.vue
@@ -1,23 +1,23 @@
 <template>
   <tab-bar>
     <tab-bar-item path="/home" activeColor="deepPink">
-      <img slot="item-icon" src="../assets/img/tabbar/home.svg">
-      <img slot="item-icon-active" src="../assets/img/tabbar/home_active.svg">
+      <img slot="item-icon" src="~assets/img/tabbar/home.svg">
+      <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg">
       <div slot="item-text">首页</div>
     </tab-bar-item>
     <tab-bar-item path="/category" activeColor="green">
-      <img slot="item-icon" src="../assets/img/tabbar/category.svg">
-      <img slot="item-icon-active" src="../assets/img/tabbar/category_active.svg">
+      <img slot="item-icon" src="~assets/img/tabbar/category.svg">
+      <img slot="item-icon-active" src="~assets/img/tabbar/category_active.svg">
       <div slot="item-text">分类</div>
     </tab-bar-item>
     <tab-bar-item path="/cart" activeColor="pink">
-      <img slot="item-icon" src="../assets/img/tabbar/shopcart.svg">
-      <img slot="item-icon-active" src="../assets/img/tabbar/shopcart_active.svg">
+      <img slot="item-icon" src="~assets/img/tabbar/shopcart.svg">
+      <img slot="item-icon-active" src="~assets/img/tabbar/shopcart_active.svg">
       <div slot="item-text">购物车</div>
     </tab-bar-item>
     <tab-bar-item path="profile" activeColor="blue">
-      <img slot="item-icon" src="../assets/img/tabbar/profile.svg">
-      <img slot="item-icon-active" src="../assets/img/tabbar/profile_active.svg">
+      <img slot="item-icon" src="~assets/img/tabbar/profile.svg">
+      <img slot="item-icon-active" src="~assets/img/tabbar/profile_active.svg">
       <div slot="item-text">我的</div>
     </tab-bar-item>
   </tab-bar>
@@ -25,8 +25,10 @@
 
 <script>
 
-  import TabBar from './tabbar/TabBar'
-  import TabBarItem from './tabbar/TabBarItem'
+  /*import TabBar from './tabbar/TabBar'
+  import TabBarItem from './tabbar/TabBarItem'*/
+  import TabBar from '@/components/tabbar/TabBar'
+  import TabBarItem from '@/components/tabbar/TabBarItem'
 
   export default {
     name: "MainTabBar",
